05e69d86808c08e618cd78f592312f6878698331
Tobias Buhl <tobias.buhl@multivac.de>
2020-07-28 13:20:14 +0200

    Initial commit

---
+++ SpecFlow_NUnit/StepDefinitions.cs
@@ -0,0 +1,33 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecFlow_xUnit
+{
+  namespace MyNamespace
+  {
+    [Binding]
+    public class StepDefinitions
+    {
+      private readonly ScenarioContext _scenarioContext;
+
+      public StepDefinitions(ScenarioContext scenarioContext)
+      {
+        _scenarioContext = scenarioContext;
+      }
+      [Given(@"a parameter named (.*)")]
+      public void GivenAParameterNamed(string name)
+      {
+        
+      }
+        
+      [When(@"testrun is started with filter")]
+      public void WhenTestrunIsStartedWithFilter()
+      {
+      }
+        
+      [Then(@"the correct tests are executed")]
+      public void ThenTheCorrectTestsAreExecuted()
+      {
+      }
+    }
+  }
+}
---
+++ SpecFlow_xUnit/Tags.feature
@@ -0,0 +1,18 @@
+﻿@Tags
+Feature: Tags
+
+  Scenario Outline: Test filters
+    Given a parameter named <name>
+    When testrun is started with filter
+    Then the correct tests are executed
+    
+    @Something
+    Examples: 
+    |name     |
+    |Something|
+    
+    @Different
+    Examples:
+    |name     |
+    |different|
+    
